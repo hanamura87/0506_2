51a02e59e677d6590c629be0b69526b5acbfd221
hanamura87 <231205143@ccmailg.meijo-u.ac.jp>
2024-05-06 16:50:58 +0900

    グリーンに変更

--- js/tetris.js
+++ js/tetris.js
@@ -22,7 +22,7 @@ var shapes = [
       1, 1, 1 ]
 ];
 var colors = [
-    'green', 'green', 'green', 'green', 'green', 'green', 'green'
+    'green', 'green', 'green', 'green', 'green', 'green', 'green' //全てグリーンに変更
 ];
 
 // creates a new 4x4 shape in global variable 'current'
